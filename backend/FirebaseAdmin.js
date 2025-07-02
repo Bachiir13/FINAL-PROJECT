23c875a355ab7679194171eeb69b3c4923928749
Bachiir13 <bachirferhane@gmail.com>
2025-07-02 15:50:15 +0200

    Description claire de tes changements

--- backend/FirebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./config/projet-final-62a74-firebase-adminsdk-fbsvc-929f4a9659.json"); // ← vérifie bien le chemin
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
