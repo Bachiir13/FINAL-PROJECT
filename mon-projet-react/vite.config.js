aa3ad61ee1a4ce87c34b8bc8302d62dfe006c012
Bachiir13 <bachirferhane@gmail.com>
2025-10-27 09:17:57 +0100

    message

--- mon-projet-react/vite.config.js
+++ mon-projet-react/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <-- important pour que les assets pointent correctement à la racine
   server: {
     proxy: {
       '/users': {
